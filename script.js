9240b43f7e6f3021f0138f4f8bd64d679676a939
Evgeniya Veselkova <evgeniyaveselk@gmail.com>
2020-12-29 20:06:08 -0500

    Adding loader

--- script.js
+++ script.js
@@ -3,22 +3,40 @@ const quoteText = document.getElementById('quote');
 const authorText = document.getElementById('author');
 const twitterBtn = document.getElementById('twitter');
 const newQuoteBtn = document.getElementById('new-quote');
+const loader = document.getElementById('loader');
 
 let apiQuotes = [];
 
+// Show loading
+function loading() {
+  loader.hidden = false;
+  quoteContainer.hidden = true;
+}
+
+// Hide Loader 
+function loadingComplete() {
+  quoteContainer.hidden = false;
+  loader.hidden = true;
+}
+
 // Show new quote
 function newQuote() {
+  loading();
   // pick a random quote from apiQuotes array
   const quote = apiQuotes[Math.floor(Math.random() * apiQuotes.length)];
   // check if author field is blank || Unknown
   (!quote.author) ? authorText.textContent = "Unknown" : authorText.textContent = quote.author;
   // check quote length to determine styling 
   (quote.text > 50) ?  quoteText.classList.add('long-quote') : quoteText.classList.remove('long-quote');
+
+  // Set a quote & hide loading
   quoteText.textContent = quote.text;
+  loadingComplete();
 }
 
 // Get quotes from API
 async function getQuotes() {
+  loading();
   const apiUrl = 'https://type.fit/api/quotes';
 
   try {

9887f51028e0450ab88662ae811275b5cef788ed
Evgeniya Veselkova <evgeniyaveselk@gmail.com>
2020-12-29 19:32:27 -0500

    Pick a random quote

--- script.js
+++ script.js
@@ -1,13 +1,20 @@
 let apiQuotes = [];
-// Get quotes from API
 
+// Show new quote
+function newQuote() {
+  // pick a random quote from apiQuotes array
+  const quote = apiQuotes[Math.floor(Math.random() * apiQuotes.length)]
+  console.log(quote);
+}
+
+// Get quotes from API
 async function getQuotes() {
   const apiUrl = 'https://type.fit/api/quotes';
 
   try {
     const response = await fetch(apiUrl);
     apiQuotes = await response.json();
-    console.log(apiQuotes);
+    newQuote();
   } catch (error) {
     // Catch error here
   }

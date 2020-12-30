e640132550d424a84a8a315554451fbb400013b3
Evgeniya Veselkova <evgeniyaveselk@gmail.com>
2020-12-29 19:39:51 -0500

    Display a dynamic quote

--- script.js
+++ script.js
@@ -1,10 +1,17 @@
+const quoteContainer = document.getElementById('quote-container');
+const quoteText = document.getElementById('quote');
+const authorText = document.getElementById('author');
+const twitterBtn = document.getElementById('twitter');
+const newQuoteBtn = document.getElementById('new-quote');
+
 let apiQuotes = [];
 
 // Show new quote
 function newQuote() {
   // pick a random quote from apiQuotes array
   const quote = apiQuotes[Math.floor(Math.random() * apiQuotes.length)]
-  console.log(quote);
+  authorText.textContent = quote.author;
+  quoteText.textContent = quote.text;
 }
 
 // Get quotes from API
